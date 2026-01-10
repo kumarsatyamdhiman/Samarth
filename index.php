3253160388a761eb4a173320f87463ebab11136d
K. Satyam <satyam.dhiman@gmail.com>
2026-01-11 01:12:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,29 +5,11 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-/*
-|--------------------------------------------------------------------------
-| Check If Application Is Under Maintenance
-|--------------------------------------------------------------------------
-*/
-
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-*/
-
 require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-*/
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
@@ -38,27 +20,3 @@ $response = $kernel->handle(
 )->send();
 
 $kernel->terminate($request, $response);
-EOF
-
-# Also create .htaccess for URL rewriting
-cat > /Users/k.satyam/Desktop/samarth/public/.htaccess << 'EOF'
-<IfModule mod_rewrite.c>
-    <IfModule mod_negotiation.c>
-        Options -MultiViews -Indexes
-    </IfModule>
-
-    RewriteEngine On
-
-    # Handle Authorization header
-    RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
-
-    # Redirect all requests to public folder
-    RewriteRule ^$ public/ [L]
-    RewriteRule ^(.*)$ public/$1 [L]
-
-    # Handle Front Controller...
-    RewriteCond %{REQUEST_FILENAME} !-d
-    RewriteCond %{REQUEST_FILENAME} !-f
-    RewriteRule ^ public/index.php [L]
-</IfModule>
-EOF
